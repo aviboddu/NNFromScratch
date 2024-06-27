c37d0273a95c46b587a7e9e28126011db5272464
aviboddu <aviboddu@gmail.com>
2024-06-27 08:30:17 +0530

    Modified layers to use Vectors

--- NNFromScratch/NeuralNet.cs
+++ NNFromScratch/NeuralNet.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
-using System.Runtime.InteropServices;
 using MathUtils;
 
 namespace NeuralNet;
@@ -29,48 +28,42 @@ public abstract class Layer
     public abstract float[] CalculateLayer(float[] input);
 }
 
+// // 0.0007 ms to CalculateOutput
 public class MatrixLayer : Layer
 {
-    private int LayerSize => matrix.GetLength(0);
-    private int InputSize => matrix.GetLength(1);
-
-    public float[,] matrix;
+    public Vector<float>[] weights;
     public float[] biases;
 
     public MatrixLayer(int layerSize, int inputSize)
     {
-        matrix = new float[layerSize, inputSize];
-        for (int i = 0; i < matrix.GetLength(0); i++)
-            for (int j = 0; j < matrix.GetLength(1); j++)
-                matrix[i, j] = Random.Shared.NextSingle();
+        weights = new Vector<float>[layerSize];
+        for (int i = 0; i < weights.Length; i++)
+            weights[i] = new(Random.Shared.NextSingles(inputSize));
 
         biases = Random.Shared.NextSingles(layerSize);
     }
 
     public override float[] CalculateLayer(float[] input)
     {
-        float[] output = new float[LayerSize];
+        float[] output = new float[weights.Length];
         Vector<float> inputVec = new(input);
-        for (int i = 0; i < LayerSize; i++)
-        {
-            Vector<float> matVec = new(MemoryMarshal.CreateReadOnlySpan(ref matrix[i, 0], InputSize));
-            output[i] = MathUtils.MathUtils.Sigmoid(Vector.Dot(matVec, inputVec) + biases[i]);
-        }
+        for (int i = 0; i < weights.Length; i++)
+            output[i] = MathUtils.MathUtils.Sigmoid(Vector.Dot(weights[i], inputVec) + biases[i]);
         return output;
     }
 }
 
+// 0.0011 ms to CalculateOutput
 public class NodeLayer : Layer
 {
     public required Node[] nodes;
 
+    [SetsRequiredMembers]
     public NodeLayer(int layerSize, int inputSize)
     {
         nodes = new Node[layerSize];
         for (int i = 0; i < layerSize; i++)
-        {
             nodes[i] = new Node(inputSize);
-        }
     }
 
     public override float[] CalculateLayer(float[] input)
@@ -85,16 +78,13 @@ public class NodeLayer : Layer
 [method: SetsRequiredMembers]
 public class Node(int num_weights)
 {
-    public required float[] weights = Random.Shared.NextSingles(num_weights);
+    readonly int num_weights = num_weights;
+    public required Vector<float> weights = new(Random.Shared.NextSingles(num_weights));
     public readonly float bias = Random.Shared.NextSingle();
 
     public float CalculateOutput(float[] input)
     {
-        Debug.Assert(input.Length == weights.Length, "input.Length != weights.Length");
-        float output = 0;
-        for (int i = 0; i < input.Length; i++)
-            output += weights[i] * input[i];
-        output += bias;
-        return MathUtils.MathUtils.Sigmoid(output);
+        Debug.Assert(input.Length == num_weights, "input.Length != weights.Length");
+        return MathUtils.MathUtils.Sigmoid(Vector.Dot(weights, new(input)) + bias);
     }
 }
