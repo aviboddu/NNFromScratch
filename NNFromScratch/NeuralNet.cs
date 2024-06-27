aac1eb36ddbcbefb00b403ab80f2e191c0b1d2a6
aviboddu <aviboddu@gmail.com>
2024-06-27 08:07:39 +0530

    Implemented Layers as both Nodes and Matrices. Matrices are 10x faster

--- NNFromScratch/NeuralNet.cs
+++ NNFromScratch/NeuralNet.cs
@@ -1,35 +1,91 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace NeuralNet;
 
 public class NeuralNet
 {
-    private readonly Node[][] neuralNet;
+    private readonly Layer[] neuralNet;
     public NeuralNet(int[] layerSizes)
     {
-        neuralNet = new Node[layerSizes.Length - 1][];
+        neuralNet = new Layer[layerSizes.Length - 1];
         for (int i = 1; i < layerSizes.Length; i++)
         {
-            neuralNet[i - 1] = new Node[layerSizes[i]];
-            for (int j = 0; j < layerSizes[i]; j++)
-                neuralNet[i - 1][j] = new(layerSizes[i - 1]);
+            neuralNet[i - 1] = new MatrixLayer(layerSizes[i], layerSizes[i - 1]);
         }
     }
 
     public float[] CalculateOutput(float[] input)
     {
         for (int i = 0; i < neuralNet.Length; i++)
-            input = CalculateLayer(input, i);
+            input = neuralNet[i].CalculateLayer(input);
         return input;
     }
 
-    public float[] CalculateLayer(float[] input, int nextLayer)
+    private static float[] RandomFloats(int num_floats)
     {
-        float[] output = new float[neuralNet[nextLayer].Length];
-        for (int i = 0; i < neuralNet[nextLayer].Length; i++)
-            output[i] = neuralNet[nextLayer][i].CalculateOutput(input);
-        return output;
+        float[] result = new float[num_floats];
+        for (int i = 0; i < num_floats; i++)
+            result[i] = Random.Shared.NextSingle();
+        return result;
+    }
+
+    private abstract class Layer
+    {
+        public abstract float[] CalculateLayer(float[] input);
+    }
+
+    private class MatrixLayer : Layer
+    {
+        public float[,] matrix;
+        public float[] biases;
+
+        public MatrixLayer(int layerSize, int inputSize)
+        {
+            matrix = new float[layerSize, inputSize];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = Random.Shared.NextSingle();
+
+            biases = RandomFloats(layerSize);
+        }
+
+        public override float[] CalculateLayer(float[] input)
+        {
+            float[] output = new float[matrix.GetLength(0)];
+            Vector<float> inputVec = new(input);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Vector<float> matVec = new(MemoryMarshal.CreateReadOnlySpan(ref matrix[i, 0], matrix.GetLength(1)));
+                output[i] = Vector.Dot(matVec, inputVec) + biases[i];
+            }
+            return output;
+        }
+    }
+
+    private class NodeLayer : Layer
+    {
+        public required Node[] nodes;
+
+        [SetsRequiredMembers]
+        public NodeLayer(int layerSize, int inputSize)
+        {
+            nodes = new Node[layerSize];
+            for (int i = 0; i < layerSize; i++)
+            {
+                nodes[i] = new Node(inputSize);
+            }
+        }
+
+        public override float[] CalculateLayer(float[] input)
+        {
+            float[] output = new float[nodes.Length];
+            for (int i = 0; i < nodes.Length; i++)
+                output[i] = nodes[i].CalculateOutput(input);
+            return output;
+        }
     }
 
     [method: SetsRequiredMembers]
@@ -46,14 +102,6 @@ public class NeuralNet
                 output += weights[i] * input[i];
             output += bias;
             return Sigmoid(output);
-        }
-
-        private static float[] RandomFloats(int num_floats)
-        {
-            float[] result = new float[num_floats];
-            for (int i = 0; i < num_floats; i++)
-                result[i] = Random.Shared.NextSingle();
-            return result;
         }
 
         private static float Sigmoid(float x) => 1 / (1 + MathF.Exp(-x));
